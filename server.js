e2e0c5ab64d77cc79a7bb0ada25bb013d699cbab
ardacanhayla5-cmd <ardacanhayla5@gmail.com>
2025-12-29 16:23:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 /* ======================
    CONFIG
 ====================== */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET || "auro_haze_secret";
 
 /* ======================
