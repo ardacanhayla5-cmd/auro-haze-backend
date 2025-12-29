d80b5514b37447c794f9e29bf2fcd5b2e3f41010
ardacanhayla5-cmd <ardacanhayla5@gmail.com>
2025-12-29 20:04:29 +0300

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -20,3 +20,14 @@ async function sendMail(to, subject, text) {
 }
 
 module.exports = { sendMail };
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: false, 
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
